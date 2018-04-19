de779a887eb5c9882b7fffb1333d1a871fde87c6
David Dormagen <czapper@gmx.de>
2018-04-19 16:03:34 +0200

    db: allow to specify port for automatic db cursors

--- bb_trajectory/db.py
+++ bb_trajectory/db.py
@@ -3,10 +3,15 @@ import math
 import numba
 import numpy as np
 
-database_host = "localhost"
+server_address = "localhost:5432"
 
 def get_database_connection(application_name="bb_trajectory"):
-    return psycopg2.connect("dbname='beesbook' user='reader' host='{}' password='reader'".format(database_host),
+    database_host = server_address
+    database_port = 5432
+
+    if ":" in database_host:
+        database_host, database_port = database_host.split(":")
+    return psycopg2.connect("dbname='beesbook' user='reader' host='{}' port='{}' password='reader'".format(database_host, database_port),
                           application_name="get_neighbour_frames")
 
 class DatabaseCursorContext(object):
