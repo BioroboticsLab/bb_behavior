053728611b831aef2e8d642ec2a044d1ec400360
David Dormagen <czapper@gmx.de>
2019-02-11 17:42:11 +0100

    add helper function to evaluate interaction classification models

---
+++ bb_behavior/trophallaxis/evaluate_models.py
@@ -0,0 +1,107 @@
+from ..utils.interaction_model_evaluation import order_bee_ids, evaluate_interaction_model
+from . import prefilter, trajectory_filter
+from ..io import biotracker
+from .. import db
+import numpy as np
+import pandas as pd
+
+def load_test_set(root_path=None):
+    root_path = root_path or "/mnt/storage/david/data/beesbook/trophallaxis/testset/"
+    import os
+    file_ending = ".annotations.json"
+    annotation_files = [f for f in os.listdir(root_path) if f.endswith(file_ending)]
+    print("Found {} annotated files.".format(len(annotation_files)))
+
+    all_annotations = []
+    annotated_frame_ids = []
+    for f_idx, f in enumerate(annotation_files):
+        filename = f.split(".")[0]
+        frame_id = int(filename.split("_")[1])
+        annotated_frame_ids.append(frame_id)
+        filename = root_path + filename
+        annotations = biotracker.load_annotated_bee_video(video_filename=filename+".mp4", object_name="detection_idx")
+        #tracks = bb_behavior.io.biotracker.load_tracks(filename+".json", using_bee_ids=False)
+        #display(annotations.head())
+        annotations["frame_id"] = frame_id
+        annotations.frame_id = annotations.frame_id.astype(np.uint64)
+        all_annotations.append(annotations)
+    annotated_frame_ids = set(annotated_frame_ids)
+    print("Total annotated frame IDs: {}".format(len(annotated_frame_ids)))
+    all_annotations = pd.concat(all_annotations, axis=0)
+    trophallaxis_annotations = all_annotations[all_annotations.comment.str.contains("trophallaxis")]
+    trophallaxis_annotations = trophallaxis_annotations[~pd.isnull(trophallaxis_annotations.bee_id1)]
+    gt_data = trophallaxis_annotations[["frame_id", "bee_id0", "bee_id1"]]
+
+    return annotated_frame_ids, gt_data
+
+def apply_prefilter_to_frame(frame_id, **kwargs):
+    _, _, _, data = prefilter.get_data_for_frame_id_high_recall(None, frame_id, None)
+    
+    return data
+
+def apply_prefilter_to_frame_sequence(frame_id, cursor=None):
+    neighbour_frames = db.get_neighbour_frames(frame_id=frame_id, n_frames=5,
+                                                           cursor=cursor, cursor_is_prepared=cursor is not None)
+    frame_ids = [f_id for (_, f_id, _) in neighbour_frames[::2]]
+    results = []
+    for frame_id in frame_ids:
+        res = apply_prefilter_to_frame(frame_id, cursor=cursor)
+        if res.shape[0] > 0:
+            res = set(order_bee_ids(b0, b1) for b0, b1 in res[["bee_id0", "bee_id1"]].itertuples(index=False))
+        else:
+            res = set()
+        results.append(res)
+    before_results = set().union(*results[:(len(results)//2)])
+    after_results  = set().union(*results[ (len(results)//2):])
+    results = list(before_results & after_results)
+    return pd.DataFrame(results, columns=["bee_id0", "bee_id1"])
+
+def apply_prefilter_and_trajfilter_to_frame(frame_id, threshold=0.5, cursor=None):
+    import torch
+    _, _, _, data = prefilter.get_data_for_frame_id_high_recall(
+                                                    None, frame_id, None, 
+                                                    cursor=cursor, cursor_is_prepared=cursor is not None)
+    X, samples, valid = trajectory_filter.load_features(data)
+    
+    model = torch.load("/mnt/storage/david/cache/beesbook/trophallaxis/1dcnn.cache")
+    Y = model.predict_proba(X)[:, 1]
+    df = []
+    for idx, (sample_idx, y) in enumerate(zip(valid, Y)):
+        if y < threshold:
+            continue
+        df.append(dict(
+            bee_id0=samples.bee_id0.iloc[sample_idx],
+            bee_id1=samples.bee_id1.iloc[sample_idx]))
+    df = pd.DataFrame(df)
+    return df
+
+def apply_prefilter_and_trajfilter_to_frame_sequence(frame_id, threshold=0.5, cursor=None):
+    neighbour_frames = db.get_neighbour_frames(frame_id=frame_id, n_frames=5,
+                                                           cursor=cursor, cursor_is_prepared=cursor is not None)
+    frame_ids = [f_id for (_, f_id, _) in neighbour_frames[::2]]
+    results = []
+    for frame_id in frame_ids:
+        res = apply_prefilter_and_trajfilter_to_frame(frame_id, threshold=threshold, cursor=cursor)
+        if res.shape[0] > 0:
+            res = set(order_bee_ids(b0, b1) for b0, b1 in res[["bee_id0", "bee_id1"]].itertuples(index=False))
+        else:
+            res = set()
+        results.append(res)
+    before_results = set().union(*results[:(len(results)//2)])
+    after_results  = set().union(*results[ (len(results)//2):])
+    results = list(before_results & after_results)
+    return pd.DataFrame(results, columns=["bee_id0", "bee_id1"])
+
+def evaluate(root_path="/mnt/storage/david/data/beesbook/trophallaxis/testset/"):
+    frame_ids, ground_truth = load_test_set()
+
+
+    evaluate_interaction_model(apply_prefilter_to_frame,
+               frame_ids, ground_truth, label="Trophallaxis Prefilter")
+    evaluate_interaction_model(apply_prefilter_to_frame_sequence,
+               frame_ids, ground_truth, label="Trophallaxis Prefilter (Multi-Frame)")
+    evaluate_interaction_model(apply_prefilter_and_trajfilter_to_frame,
+               frame_ids, ground_truth, label="Trajectory Filter")
+    evaluate_interaction_model(apply_prefilter_and_trajfilter_to_frame_sequence,
+               frame_ids, ground_truth, label="Trajectory Filter (Multi-Frame)")
+    
---
+++ bb_behavior/utils/interaction_model_evaluation.py
@@ -0,0 +1,101 @@
+from collections import defaultdict
+import numpy as np
+import pandas as pd
+
+from ..db import DatabaseCursorContext, find_interactions_in_frame
+
+def order_bee_ids(b0, b1):
+    return min(b0, b1), max(b0, b1)
+
+
+def evaluate_interaction_model(model_fun, frame_ids, gt_df, label="Model Evaluation"):
+    """Takes an evaluation function and ground truth and assesses the performance of the function.
+
+    Arguments:
+        model_fun: callable
+            Function of the form (frame_id, cursor) -> pandas.DataFrame[["bee_id0", "bee_id1"]].
+        frame_ids: list(int)
+            List of frame IDs for which to call model_fun.
+        gt_df: pandas.DataFrame
+            DataFrame containing at least the columns 'frame_id, 'bee_id0', 'bee_id1'.
+            Evaluated against model_fun.
+    """
+    results = defaultdict(int)
+    
+    with DatabaseCursorContext() as cursor:
+        
+        for frame_id in frame_ids:
+            
+            
+            # Ground truth.
+            df = gt_df[gt_df.frame_id == frame_id]
+            
+            pairs = list()
+            for bee_id0, bee_id1 in df[["bee_id0", "bee_id1"]].itertuples(index=False):
+                pairs.append(order_bee_ids(bee_id0, bee_id1))
+            pairs = set(pairs)
+            
+            # Prediction.
+            frame_results = model_fun(frame_id, cursor=cursor)
+            try:
+                frame_results = set(order_bee_ids(*p) for p in frame_results[["bee_id0", "bee_id1"]].itertuples(index=False))
+            except:
+                frame_results = set()
+                print("Warning: Frame results emtpy. (Frame {})".format(frame_id))
+            
+            # Total interactions.
+            interactions = find_interactions_in_frame(frame_id, max_distance=40.0,
+                                                                     confidence_threshold=0.0,
+                                                                     cursor=cursor, cursor_is_prepared=True)
+            all_candidates = set(order_bee_ids(inter[1], inter[2]) for inter in interactions)                                                        
+            assert (len(pairs & all_candidates) == len(pairs))
+            negative_pairs = all_candidates - pairs
+            
+            P = len(pairs)
+            N = len(negative_pairs)
+            TP = len(frame_results & pairs)
+            FP = len(frame_results) - TP
+            TN = len(negative_pairs - pairs - frame_results)
+            FN = len(pairs - frame_results)
+
+            results["P"] += P
+            results["N"] += N
+            results["TP"] += TP
+            results["FP"] += FP
+            results["TN"] += TN
+            results["FN"] += FN
+
+    P, TP, FP = results["P"], results["TP"], results["FP"]
+    N, TN, FN = results["N"], results["TN"], results["FN"]
+    TPR = TP / P # Recall, hit rate
+    TNR = TN / N # Specificity
+    PPV = TP / (TP + FP) # Precision
+    NPV = TN / (TN + FN) # Neg. pred. val
+    F1  = np.nan
+    try:
+        F1 = 2 * (PPV * TPR) / (PPV + TPR)
+    except:
+        pass
+    
+    results = (f"{label}\n"
+                "==============================\n"
+               f"Recall     (TPR): {TPR:6.2%}\n"
+               f"Precision  (PPV): {PPV:6.2%}\n"
+               f"F1-Score   ( F1): {F1:6.2%}\n"
+               f"Specificity(TNR): {TNR:6.2%}\n"
+               f"Neg.Pred.V.(NPV): {NPV:6.2%}\n"
+               "\n"
+               f"Positive   (  P): {P:6d}\n"
+               f"True Pos.  ( TP): {TP:6d}\n"
+               f"False Pos. ( FP): {FP:6d}\n"
+               "\n"
+               f"Negative   (  N): {N:6d}\n"
+               f"True Neg.  ( TN): {TN:6d}\n"
+               f"False Neg. ( FN): {FN:6d}\n"
+               f"Frames          : {len(frame_ids):6d}\n"
+              )
+    print(results)
+            
+            
+            
+        
