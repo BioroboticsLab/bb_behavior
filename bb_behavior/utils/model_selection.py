706cab327023ecf74b5582d6e40d2bcf4485d5d9
David Dormagen <czapper@gmx.de>
2019-02-14 18:24:23 +0100

    utils.iterate_minibatches: allow passing the length of the structure to be able to handle non-numpy arrays

--- bb_behavior/utils/model_selection.py
+++ bb_behavior/utils/model_selection.py
@@ -5,13 +5,15 @@ import sklearn.metrics
 import matplotlib.pyplot as plt
 
 # source: https://stackoverflow.com/a/38176770
-def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
+def iterate_minibatches(inputs, targets, batchsize, N=None, shuffle=False):
+    if N is None:
+        N = inputs.shape[0]
     if targets is not None:
         assert inputs.shape[0] == targets.shape[0]
     if shuffle:
-        indices = np.arange(inputs.shape[0])
+        indices = np.arange(N)
         np.random.shuffle(indices)
-    for start_idx in range(0, inputs.shape[0] - batchsize + 1, batchsize):
+    for start_idx in range(0, N - batchsize + 1, batchsize):
         if shuffle:
             excerpt = indices[start_idx:start_idx + batchsize]
         else:
