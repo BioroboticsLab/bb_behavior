85f6cc9694ec4d3d9fb7865ebdb2dc623af51f46
David Dormagen <czapper@gmx.de>
2019-05-29 16:39:07 +0200

    db.get_database_connection: support different user/password

--- bb_behavior/db/base.py
+++ bb_behavior/db/base.py
@@ -1,12 +1,17 @@
 import psycopg2
 
 server_address = "localhost:5432"
+database_user = "reader"
+database_password = "reader"
 
-def get_database_connection(application_name="bb_behavior"):
+def get_database_connection(application_name="bb_behavior", user=None, password=None):
     database_host = server_address
     database_port = 5432
-
+    if user is None:
+        user = database_user
+    if password is None:
+        password = database_password
     if ":" in database_host:
         database_host, database_port = database_host.split(":")
-    return psycopg2.connect("dbname='beesbook' user='reader' host='{}' port='{}' password='reader'".format(database_host, database_port),
+    return psycopg2.connect("dbname='beesbook' user='{}' host='{}' port='{}' password='{}'".format(user, database_host, database_port, password),
                           application_name=application_name)
