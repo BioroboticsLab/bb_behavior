38f9d9b218d8aac825002acb2f5ca7868712981c
David Dormagen <czapper@gmx.de>
2019-07-03 11:47:23 +0200

    trophallaxis.prefilter: adjust min/max distance to reflect new training data The max. distance is off by such a large margin because of one wrong sample.

--- bb_behavior/trophallaxis/prefilter.py
+++ bb_behavior/trophallaxis/prefilter.py
@@ -71,7 +71,7 @@ def get_data_for_frame_id(timestamp, frame_id, cam_id,
 @numba.njit
 def probability_distance_fun(xy0, xy1):
     return probability_distance_fun_(xy0, xy1, 2.04332357, -1.56938987, 1.92212738, -11.87978937,
-                                   6.843017734527588, 28.133578964233394)
+                                   7.31, 12.04) # The min/max distance are 99 percentiles.
 high_recall_threshold = 0.45398181  # 85% recall, 18% precision
 
 def get_data_for_frame_id_high_recall(*args, min_distance=high_recall_threshold, 
